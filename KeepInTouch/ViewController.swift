2bb2337182788761f0757b321e0f657e7d467422
Patricia Au <patau@uw.edu>
2018-02-06 16:33:06 -0800

    Initial Commit

---
+++ KeepInTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeepInTouch
+//
+//  Created by Patricia Au on 2/6/18.
+//  Copyright © 2018 Capstone B09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
